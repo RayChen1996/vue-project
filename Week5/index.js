2eee385194fddd5c4554d8e1b7f292d3330c303d
RayChen <ray.10315332@gmail.com>
2023-02-12 14:06:40 +0800

    Week 5 LV1 fin

--- Week5/index.js
+++ Week5/index.js
@@ -1,94 +1,165 @@
-const BaseUrl = `https://vue3-course-api.hexschool.io/v2`
-const apiPath = 'raychen';
-import BSModal from './BSModal.js'
+const { createApp } = Vue;//解構寫法
+const BaseUrl = "https://vue3-course-api.hexschool.io/v2";
+const API_PATH = "raychen";
+Object.keys(VeeValidateRules).forEach((rule) => {
+  if (rule !== "default") {
+    VeeValidate.defineRule(rule, VeeValidateRules[rule]);
+  }
+});
+// 讀取外部的資源
+VeeValidateI18n.loadLocaleFromURL("./zh_TW.json");
 
-const app = Vue.createApp({
-    data(){
-        return{
-            // tmp:[1,2,3,4,5],
-            products:[],
-            cart:[],
-            DetailPageLoadingStatus: {
-                LoadingProduct:'',
-            },
-            product: {},
-            ShoppingCartForm: {
-                user: {
-                  name: '',
-                  email: '',
-                  tel: '',
-                  address: '',
+VeeValidate.configure({
+  generateMessage: VeeValidateI18n.localize("zh_TW"),
+  validateOnInput: true, // 調整為：輸入文字時，就立即進行驗證
+});
+
+
+createApp({
+  data() {
+    return {
+      products: [],
+      cart: {},
+      more: {},
+      orderCount: 1,
+      index: 0,
+      myModal: "",
+      isLoading: false,
+      form: {
+        email: "",
+        name: "",
+        phone: "",
+        address: "",
+        message: "",
+      },
+    };
+  },
+  methods: {
+    getProducts() {
+      axios
+      .get(`${BaseUrl}/api/${API_PATH}/products`)
+      .then((res) => {
+        this.products = res.data.products;
+      });
+    },
+    getCart() {
+      axios
+      .get(`${BaseUrl}/api/${API_PATH}/cart`)
+      .then((res) => {
+        this.cart = res.data.data;
+        this.index = this.cart.carts.length;
+      });
+    },
+    showSweetAlert(message){
+        Swal.fire({
+            position: 'top-end',
+            icon: 'success',
+            title: message,
+            showConfirmButton: false,
+            timer: 1500
+        })
+    },
+    viewMore(item) {
+      this.isLoading = true;
+      this.more = item;
+      this.myModal.show();
+      setTimeout(() => {
+        this.isLoading = false;
+      }, 1000);
+    },
+    AddCart() {
+      const cart = {
+        product_id: this.more.id,
+        qty: this.orderCount,
+      };
+      axios
+      .post(`${BaseUrl}/api/${API_PATH}/cart`, { data: cart })
+      .then((res) => {
+        this.getCart();
+      });
+      this.myModal.hide();
+    },
+    joinCart(item, qty = 1) {//數量:參數預設值1
+      this.isLoading = true;
+      const cart = {
+        product_id: item.id,
+        qty,
+      };
+      axios
+      .post(`${BaseUrl}/api/${API_PATH}/cart`, { data: cart })
+      .then((res) => {
+        this.getCart();
+        this.showSweetAlert("已加入");
+        this.isLoading = false;
+      });
+    },
+    updateCart(item) {
+      const cart = {
+        product_id: item.product_id,
+        qty: item.qty,
+      };
+      axios
+        .put(`${BaseUrl}/api/${API_PATH}/cart/${item.id}`,
+          { 
+            data: cart 
+          })
+        .then((res) => {
+          this.getCart();
+        });
+    },
+    delCart(item) {
+      this.isLoading = true;
+      axios
+      .delete(`${BaseUrl}/api/${API_PATH}/cart/${item}`)
+      .then((res) => {
+        this.cart.carts.forEach((element, index) => {
+          if (item == element.id) {
+            this.cart.carts.splice(index, 1);
+          }
+        });
+        this.getCart();
+        this.showSweetAlert("已刪除");
+        this.isLoading = false;
+      });
+    },
+    clearCart() {
+      axios
+      .delete(`${BaseUrl}/api/${API_PATH}/carts/`)
+      .then((res) => {
+        this.getCart();
+        this.showSweetAlert("已清空");
+      });
+    },
+    createOrder() {
+        axios
+        .post(`${BaseUrl}/api/${API_PATH}/order`, { 
+            data: {
+                "user":{
+                    "name":this.form.name,
+                    "email":this.form.email,
+                    "tel":this.form.phone,
+                    "address":this.form.address
                 },
-                message: '',
-            },
-        }
+                "message":this.form.message
+            }
+        })
+        .then((res) => {
+            this.getCart();
+            this.showSweetAlert("已送出訂單");
+            this.isLoading = false;
+            this.$refs.form.reset();
+        })
+        .catch((err)=>{
+            console.log(err.data.message);
+        })
     },
-    methods: {
-        //獲取所有產品API
-        getAllProducts(){
-          const url = `${BaseUrl}/api/${apiPath}/products`;///v2/api/{api_path}/products
-          axios.get(url).then((response) => {
-            this.products = response.data;
-            console.log(response.data);
-            console.log(response.data.products);
-          }).catch((err) => {
-            alert(err.response.data.message);
-          });
-        },
-        viewSingleProductDetail(productId){
-          //https://vue3-course-api.hexschool.io/v2/api/raychen/product/4
-          const url = `${BaseUrl}/api/${apiPath}/product/${productId}`;///v2/api/{api_path}/products
-          axios.get(url).then((response) => {
-            // this.product = response.data;
-            // console.log(response.data);
-            // console.log(response.data.product);
-          }).catch((err) => {
-            alert(err.response.data.message);
-          });
-
-
-        },
-        //獲取下方購物車列表
-        getUserCart() {
-            const url = `${BaseUrl}/api/${apiPath}/cart`;
-            axios.get(url).then((response) => {
-              this.cart = response.data.data;
-            }).catch((err) => {
-              alert(err.response.data.message);
-            });
-        },
-        //點進會彈出Modal詳細頁
-        clickDetailProductPage(productId){
-          //https://vue3-course-api.hexschool.io/v2/api/raychen/product/4
-          const url = `${BaseUrl}/api/${apiPath}/product/${productId}`;///v2/api/{api_path}/products
-          axios.get(url).then((response) => {
-            console.log(response)
-            // this.product = response.data;
-        
-            
-            this.$refs.BSModal.openModal()
-          }).catch((err) => {
-            alert(err);
-          });
-        },
-        //新增訂單
-        newOrder(){
-
-        },
-        //刪除下方購物車清單
-        deleteCartItem(){
-
-        },
-
-
-    },
-    mounted() {
-        //先獲得上方產品列表
-        this.getAllProducts();
-        //
-        // getUserCart();
-    },
-
-})
-.component('BSModal',BSModal)
-.mount("#app");
+  },
+  mounted() {
+    this.getProducts();
+    this.getCart();
+    this.myModal = new bootstrap.Modal(document.getElementById("productModal"));
+  },
+}).component("VForm", VeeValidate.Form)
+  .component("VField", VeeValidate.Field)
+  .component("ErrorMessage", VeeValidate.ErrorMessage)
+  .mount("#app");
---
+++ Week5/old/index_Test.js
@@ -0,0 +1,94 @@
+const BaseUrl = `https://vue3-course-api.hexschool.io/v2`
+const apiPath = 'raychen';
+import BSModal from '../BSModal.js'
+
+const app = Vue.createApp({
+    data(){
+        return{
+            // tmp:[1,2,3,4,5],
+            products:[],
+            cart:[],
+            DetailPageLoadingStatus: {
+                LoadingProduct:'',
+            },
+            product: {},
+            ShoppingCartForm: {
+                user: {
+                  name: '',
+                  email: '',
+                  tel: '',
+                  address: '',
+                },
+                message: '',
+            },
+        }
+    },
+    methods: {
+        //獲取所有產品API
+        getAllProducts(){
+          const url = `${BaseUrl}/api/${apiPath}/products`;///v2/api/{api_path}/products
+          axios.get(url).then((response) => {
+            this.products = response.data;
+            console.log(response.data);
+            console.log(response.data.products);
+          }).catch((err) => {
+            alert(err.response.data.message);
+          });
+        },
+        viewSingleProductDetail(productId){
+          //https://vue3-course-api.hexschool.io/v2/api/raychen/product/4
+          const url = `${BaseUrl}/api/${apiPath}/product/${productId}`;///v2/api/{api_path}/products
+          axios.get(url).then((response) => {
+            // this.product = response.data;
+            // console.log(response.data);
+            // console.log(response.data.product);
+          }).catch((err) => {
+            alert(err.response.data.message);
+          });
+
+
+        },
+        //獲取下方購物車列表
+        getUserCart() {
+            const url = `${BaseUrl}/api/${apiPath}/cart`;
+            axios.get(url).then((response) => {
+              this.cart = response.data.data;
+            }).catch((err) => {
+              alert(err.response.data.message);
+            });
+        },
+        //點進會彈出Modal詳細頁
+        clickDetailProductPage(productId){
+          //https://vue3-course-api.hexschool.io/v2/api/raychen/product/4
+          const url = `${BaseUrl}/api/${apiPath}/product/${productId}`;///v2/api/{api_path}/products
+          axios.get(url).then((response) => {
+            console.log(response)
+            // this.product = response.data;
+        
+            
+            this.$refs.BSModal.openModal()
+          }).catch((err) => {
+            alert(err);
+          });
+        },
+        //新增訂單
+        newOrder(){
+
+        },
+        //刪除下方購物車清單
+        deleteCartItem(){
+
+        },
+
+
+    },
+    mounted() {
+        //先獲得上方產品列表
+        this.getAllProducts();
+        //
+        // getUserCart();
+    },
+
+})
+.component('BSModal',BSModal)
+.mount("#app");
